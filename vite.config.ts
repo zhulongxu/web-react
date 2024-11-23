e398140879235337bdb06590ea0d8d90847e4909
zhulongxu <3135421715@qq.com>
2024-11-23 07:39:13 +0000

    dist2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react'
 import * as path from "path";
 export default defineConfig({
   base:'./',
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
