21ebba87138287a3a6e13b0107b87f33d9276803
zhulongxu <3135421715@qq.com>
2024-11-23 10:33:28 +0800

    delNode_modules

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['react-app'],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-react': 'off'
-  }
-} 
--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/devbox/project
-npm run start
